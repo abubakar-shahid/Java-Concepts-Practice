dbc1693d1bee537e533f0756f286aeaa6369d4f3
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-29 01:32:32 +0500

    Client Added

--- src/SocketProgramming/Client.java
+++ src/SocketProgramming/Client.java
@@ -1,4 +1,35 @@
 package SocketProgramming;
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+/**
+ * @author anosha khan
+ */
+//192.168.1.104
 public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket("192.168.98.58", 80);
+        DataInputStream din = new DataInputStream(s.getInputStream());
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = "", str2 = "";
+        while (!str.equals("stop")) {
+            str = br.readLine();
+            dout.writeUTF(str);
+            dout.flush();
+            System.out.println("waiting fo server reply");
+            str2 = din.readUTF();
+            System.out.println("Server says: " + str2);
+        }
+        dout.close();
+        s.close();
+
+    }
 }
