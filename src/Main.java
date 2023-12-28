2d80cb4679de503e6651d5d3cfb04bd7a41db4ac
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-28 18:02:41 +0500

    Changes in main to invoke Q7 of Lab

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String args[]){
         System.out.println("Hello World!");
+        Q7 obj = new Q7();
+        obj.createDrawingProgram();
     }
 }
