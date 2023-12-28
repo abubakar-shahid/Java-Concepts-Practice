644b1658600dca3f35913500ccd572fdf6a3dc57
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-28 20:00:49 +0500

    Changes in Hash Map code

--- src/HashMapIteratorExample.java
+++ src/HashMapIteratorExample.java
@@ -5,20 +5,17 @@ import java.util.Set;
 
 public class HashMapIteratorExample {
     public static void main(String[] args) {
-        // Create a HashMap
+
         Map<String, Integer> hashMap = new HashMap<>();
         hashMap.put("One", 1);
         hashMap.put("Two", 2);
         hashMap.put("Three", 3);
         hashMap.put("Four", 4);
 
-        // Get the set of entries
         Set<Map.Entry<String, Integer>> entrySet = hashMap.entrySet();
 
-        // Create an iterator for the entry set
         Iterator<Map.Entry<String, Integer>> iterator = entrySet.iterator();
 
-        // Iterate and print the key-value pairs
         while (iterator.hasNext()) {
             Map.Entry<String, Integer> entry = iterator.next();
             String key = entry.getKey();
