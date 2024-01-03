f05062df7ce0e33379a8653e6c947bbdd203f1ec
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2024-01-03 14:35:59 +0500

    Client Added for SPQ1

---
+++ src/SocketProgramming1/Client.java
@@ -0,0 +1,25 @@
+package SocketProgramming1;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket("192.168.8.149", 5000);
+        DataInputStream din = new DataInputStream(s.getInputStream());
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = "", str2 = "";
+        while (!str.equals("stop")) {
+            str = br.readLine();
+            dout.writeUTF(str);
+            dout.flush();
+            System.out.println("waiting fo server reply");
+            str2 = din.readUTF();
+            System.out.println("Server says: " + str2);
+        }
+        dout.close();
+        s.close();
+    }
+}
